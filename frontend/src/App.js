54f6b90985242418c6da8cd88d77d2d7fb9689c3
= <tdhuan91ntk@gmail.com>
2021-02-07 11:46:26 -0800

    Add bootstrap.min.css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 //function App() {
 //or
 const App = () => {
   return (
     <>
-      <h1>Welcome To ProShop</h1>
+    <Header />
+    <main>
+      <Container>
+        <h1>Welcome To ProShop</h1>
+      </Container>
+    </main>
+    <Footer />
     </>
   );
 }
