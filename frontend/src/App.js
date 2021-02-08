6d732701a7572d1bf6f894aaeafbe213812695b0
= <tdhuan91ntk@gmail.com>
2021-02-07 16:57:30 -0800

    bootstrap setup, header, footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,13 +8,13 @@ import Footer from './components/Footer'
 const App = () => {
   return (
     <>
-    <Header />
-    <main>
-      <Container>
-        <h1>Welcome To ProShop</h1>
-      </Container>
-    </main>
-    <Footer />
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome To ProShop</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 }
